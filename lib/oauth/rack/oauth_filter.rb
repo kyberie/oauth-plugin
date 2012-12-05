e09fd97681517724da85baca30faf05b19585aaf
Pavel Svoboda <svoboda.pavel@gmail.com>
2012-12-05 11:10:17 +0100

    Fixed queries when using mongoid

--- lib/oauth/rack/oauth_filter.rb
+++ lib/oauth/rack/oauth_filter.rb
@@ -24,7 +24,8 @@ module OAuth
         env["oauth_plugin"] = true
         strategies = []
         if token_string = oauth2_token(request)
-          if token = Oauth2Token.first(:conditions => ['invalidated_at IS NULL AND authorized_at IS NOT NULL and token = ?', token_string])
+          if token = Oauth2Token.where(:invalidated_at=>nil, :authorized_at.ne=>nil, :token => token_string).first
+          #if token = Oauth2Token.first(:conditions => ['invalidated_at IS NULL AND authorized_at IS NOT NULL and token = ?', token_string])
             env["oauth.token"]   = token
             env["oauth.version"] = 2
             strategies << :oauth20_token
